38259510db1b32885c7046de80e37518f662d35b
Jake Neiheisel <jneiheisel98@gmail.com>
2023-10-22 10:12:53 -0400

    redo recommender.py

--- recommender.py
+++ recommender.py
@@ -1,13 +1,23 @@
-
-
-import csv
 import pandas as pd
-import numpy as np 
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+
 # reading csv file 
 df = pd.read_csv("imdb_top_1000.csv")
 df.info()
 df = df.dropna(subset=['Certificate'])
+df = df.dropna(subset=['Series_Title'])
 df = df.dropna(subset=['Runtime'])
+df = df.dropna(subset=['Genre'])
+df = df.dropna(subset=['IMDB_Rating'])
+df = df.dropna(subset=['Meta_score'])
 df.loc[df["Certificate"] == "S", "Certificate"] = "R"
 df.loc[df["Certificate"] == "Unrated", "Certificate"] = "R"
 df.loc[df["Certificate"] == "A", "Certificate"] = "R"
@@ -22,44 +32,115 @@ df.loc[df["Certificate"] == "U", "Certificate"] = "G"
 df.loc[df["Certificate"] == "GP", "Certificate"] = "PG"
 df.loc[df["Certificate"] == "Passed", "Certificate"] = "R"
 df.loc[df["Certificate"] == "Approved", "Certificate"] = "R"
-genres=[]
+     
 
-for counting in range(0, len(df['Genre'].unique())):
-    for ind in range(0, len(df['Genre'].unique()[counting].split(","))):
-        genres.append((df['Genre'].unique()[counting].split(",")[ind]).strip())
-genres = np.array(genres)
-genres = np.unique(genres)
-for counter in range(0, len(df)):
     
-    if len(df.iloc[counter][5].split(",")) > 1:
-        dataFr = []
-        for ctr in range(0, len(df.iloc[counter][5].split(","))):
-           
-            for ind in range(0,5):
-                dataFr.append(df.iloc[counter][ind])
-            dataFr.append((df.iloc[counter][5].split(",")[ctr].strip()))
-            for ind in range(6,len(df.iloc[counter])):
-                dataFr.append(df.iloc[counter][ind])
-        # dataFr = pd.DataFrame(np.array(dataFr))
-            df.add(dataFr)
-            
-            dataFr = []
-        df.iloc[counter][5] = df.iloc[counter][5].split(",")[0]
+
+    
+
+df = df.drop(columns=["Poster_Link", "Released_Year", "Overview", "Director", "Star1", "Star2", "Star3", "Star4", "No_of_Votes", "Gross"])
+df[['Genre1','Genre2', 'Genre3']] = df.Genre.str.split(expand=True)
+df['Genre1'] = df['Genre1'].str.replace(',','')
+df['Genre2'] = df['Genre2'].str.replace(',','')
+df['Genre3'] = df['Genre3'].str.replace(',','')
+
+
+print(df)
+one_df = pd.get_dummies(df, columns = ["Certificate"])
+
+one_df[['Runtime','fluff']] = one_df.Runtime.str.split(expand=True)
+
+one_df = one_df.drop(columns = ["Series_Title", "fluff", "Genre"])
+print(one_df)
+one_df["Genre1"][one_df["Genre1"].notnull()] = 1
+gen1 = one_df["Genre1"]
+gen1.fillna(value = "0", inplace=True)
+
+
+one_df["Genre2"][one_df["Genre2"].notnull()] = 1
+gen2 = one_df["Genre2"]
+gen2.fillna(value = "0", inplace=True)
+
+
+one_df["Genre3"][one_df["Genre3"].notnull()] = 1
+gen3 = one_df["Genre3"]
+gen3.fillna(value = "0", inplace=True)
+
+
 ovrScore = []
-for counter in range(0, len(df)):
+
+for idx in one_df.index:
+        
     score = 0
-    counter = 1
-    for x in range (0, len(genres)):
-        if genres[x] == (df.iloc[counter][5]):
-            break
-        counter+=1
-    score=100*counter
-    if(df.iloc[counter][3] == "PG"):
+    if one_df["Genre1"][idx] == 1 and one_df["Genre2"][idx] == 1 and one_df["Genre3"][idx] == 1:
         score += 10
-    elif (df.iloc[counter][3] == "PG-13"):
+        
+    if one_df["Genre1"][idx] == 1 and one_df["Genre2"][idx] == 1 and one_df["Genre3"][idx] == 0:
+        score += 7
+        
+    if one_df["Genre1"][idx] == 1 and one_df["Genre2"][idx] == 0 and one_df["Genre3"][idx] == 0:
+        score += 5
+        
+    if one_df['Certificate_G'][idx] == 1:
+        score += 10
+        
+    if one_df['Certificate_PG'][idx] == 1:
         score += 20
-    elif (df.iloc[counter][3] == "R"):
+                
+    if one_df['Certificate_PG-13'][idx] == 1:
         score += 30
-    elif (df.iloc[counter][3] == "G"):
+                
+    if one_df['Certificate_R'][idx] == 1:
         score += 40
-    # index = genres.index(df.iloc[counter][5])
+    ovrScore.append(score)    
+            
+print(len(ovrScore))
+one_df["Overall_score"] = ovrScore
+print(one_df)
+
+X_train, X_test, y_train, y_test = train_test_split(one_df.iloc[:,:10],one_df.iloc[:,10], random_state = 42,test_size=0.20, shuffle= True)
+
+n = np.arange(1, 16)
+train_accuracy = np.empty(len(n))
+test_accuracy = np.empty(len(n))
+  
+for i, k in enumerate(n):
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)    
+    train_accuracy[i] = knn.score(X_train, y_train)
+    test_accuracy[i] = knn.score(X_test, y_test)
+    
+print("\nTraining Accuracy for 15-NN:",train_accuracy[14])
+print("\nTest Accuracy for 15-NN:",test_accuracy[14])
+
+
+model = MLPClassifier(hidden_layer_sizes= 50, activation='relu', max_iter=10000)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test,y_pred)
+print("\nMetrics for Neural Network model:")
+print("(a) \nMean square error:", mse)
+print("(b) Root Mean Square Error(RMSE):", math.sqrt(mse))
+print("(c) Mean Square Error(MSE):", mse)
+print("(d) R^2 score:", r2_score(y_test,y_pred))
+print("(e) Mean Absolute Error(MAE):", mean_absolute_error(y_test,y_pred))
+
+
+
+plt.plot(n, train_accuracy, label = 'Training Accuracy')
+plt.plot(n, test_accuracy, label = 'Testing Accuracy', linestyle= 'dashed')
+plt.legend()
+plt.xlabel('n_neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+           
+plt.scatter(y_test, y_pred, color='blue')
+plt.title(f'Predicted vs. Actual (MSE: {mse:.2f})')
+plt.xlabel('Actual Values')
+plt.ylabel('Predicted Values')
+x = np.linspace(0, 50, 100)
+y = x
+plt.plot(x, y, linewidth=2.0)
+plt.show()
+
